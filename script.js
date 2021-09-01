b4a000dac8a99c88dc2814085f40e02ad8718a03
grindser <46396272+grindser@users.noreply.github.com>
2021-09-01 10:46:40 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const end= new Date()
+const start = new Date(2021, 8, 30, 20, 13)
+
+var diff = end.getTime() - start.getTime() / (1000 * 3600 * 24;
+console.log(diff)
+                                              
+// To calculate the no. of days between two dates
+// var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
